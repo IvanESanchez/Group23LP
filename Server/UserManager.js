6b11e6c4ab5ab05d7a72e28fe9af24d31cc1fdc6
Spirit Xela <morse.alex.j@gmail.com>
2020-07-01 20:18:48 -0400

    Setup skeleton for register endpoint

--- Server/UserManager.js
+++ Server/UserManager.js
@@ -9,13 +9,34 @@ const mongo = require("./MongoHandler.js");
 var express = require("express");
 var router = express.Router();
 
+function doesUserExist(username, email) {
+	return false;
+}
+function createUser(username, password, email) {
+
+}
 router.post("/register",  async (req, res, next) => {
-	console.log(req.body);
+	//Make sure body exists
+	if (req.body == null) {
 
-	//TODO
+	}
 
-	var ret = {message:"Done"};
-	res.status(200).json(ret);
+	//Collect information from body
+	var username = req.body.username;
+	var password = req.body.password;
+	var email = req.body.email;
+
+	//Make sure body info exists
+	if (username == null || password == null || email == null) {
+		//Body was formatted incorrectly, return error code 400
+		res.status(400);
+	}
+
+	if (!doesUserExist(username, email)) {
+		createUser(username, password, email);
+	}
+
+	res.status(200);
 });
 
 router.post("/login", async (req, res, next) => {
