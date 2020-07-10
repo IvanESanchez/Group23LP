b008c2a4181c6f7714b7f73f16dc7c4671314a00
Julian Braha <julianbraha@gmail.com>
2020-07-10 02:48:33 -0400

    Add initial UI for unit converter page

--- flutter_project/my_recipe_box/lib/converter.dart
+++ flutter_project/my_recipe_box/lib/converter.dart
@@ -1,35 +1,116 @@
 import 'package:flutter/material.dart';
 
-import 'package:unit_converter/unit_converter.dart'; // for usage, see: https://pub.dev/packages/unit_converter
+//import 'package:unit_converter/unit_converter.dart'; // for usage, see: https://pub.dev/packages/unit_converter
 
-class Converter extends StatelessWidget {
+class Converter extends StatefulWidget {
+  Converter({Key key}) : super(key: key);
+
+  @override
+  _ConverterState createState() => _ConverterState();
+}
+
+class _ConverterState extends State<Converter> {
   @override
   Widget build(BuildContext context) {
+    //List<String> units = <String>["Unit Placeholder 1", "Unit Placeholder 2", "Unit Placeholder 3"]; // TODO: add more
+    String dropdownValue = "Teaspoons";
     return Container(
-        color: Colors.green[50], // change this color to change the background color
-        child: Scaffold(
-          //backgroundColor: Colors.white,
-          body: Container(
-            color: Colors.green[50], // change this color to change the background color
-            child: Center(
-              child: Column(
-                // center the children
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: <Widget>[
-                  Icon(
-                    Icons.loop,
-                    size: 160.0,
-                    color: Colors.grey,
-                  ),
-                  Text(
-                    "Converter",
-                    style: TextStyle(color: Colors.grey),
-                  )
-                ],
-              ),
+
+
+      child: Column(
+
+
+        mainAxisSize: MainAxisSize.min,
+        children: <Widget>[
+
+            ListTile(
+              // contentPadding: , // TODO: add padding
+
+              //leading: Icon(Icons.local_dining), // NOTE: could add an icon here
+              //title: Text("\n${units[index]}"),
+              subtitle: Text(
+                  "\nPlease enter the amount you would like to convert:",
+                  style: TextStyle(color: Colors.black, fontWeight: FontWeight.bold, fontSize: 20)),
+            ),
+
+            ListTile( // NOTE: DROPDOWN 1
+                title: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: <Widget> [
+
+                    DropdownButton(
+
+                      value: dropdownValue,
+                      //icon: Icon(Icons.arrow_downward), NOTE: could change the icon in the dropdown here
+                      iconSize: 24,
+                      elevation: 16,
+
+                      style: TextStyle(color: Colors.black), // NOTE: This is the color of the dropdown menu text
+                      underline: Container(
+                        height: 2,
+                        color: Colors.green, // NOTE: this is the color of the underline of the dropdown
+                      ),
+                      onChanged: (String newValue) {
+                        setState(() {
+                          dropdownValue = newValue;
+                        });
+                      },
+                      items: <String>['Teaspoons', 'Tablespoons', 'Cups', 'Pints'].map<DropdownMenuItem<String>>((String value) {
+                        return DropdownMenuItem<String>(
+                          value: value,
+                          child: Text(value),
+                        );
+                      }).toList(),
+                    ),
+
+                ]
+            )
+
+            ),
+
+          ListTile( // NOTE: DOWN ARROW
+            title: Icon(
+                Icons.arrow_downward,
+                color: Colors.black,
             ),
           ),
-        )
+          ListTile( // NOTE: DROPDOWN 2
+              title: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: <Widget> [
+
+                    DropdownButton(
+
+                      value: dropdownValue,
+                      //icon: Icon(Icons.arrow_downward), NOTE: could change the icon in the dropdown here
+                      iconSize: 24,
+                      elevation: 16,
+
+                      style: TextStyle(color: Colors.black), // NOTE: This is the color of the dropdown menu text
+                      underline: Container(
+                        height: 2,
+                        color: Colors.green, // NOTE: this is the color of the underline of the dropdown
+                      ),
+                      onChanged: (String newValue) {
+                        setState(() {
+                          dropdownValue = newValue;
+                        });
+                      },
+                      items: <String>['Teaspoons', 'Tablespoons', 'Cups', 'Pints'].map<DropdownMenuItem<String>>((String value) {
+                        return DropdownMenuItem<String>(
+                          value: value,
+                          child: Text(value),
+                        );
+                      }).toList(),
+                    ),
+
+                  ]
+              )
+
+          ),
+        ],
+      ),
+
     );
   }
 }
