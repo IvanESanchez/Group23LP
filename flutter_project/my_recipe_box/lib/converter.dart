90c00730ff3f330a16bcc1b66303e2c71850d1fc
Julian Braha <julianbraha@gmail.com>
2020-07-14 09:54:32 -0400

    Added final polish to unit converter

--- flutter_project/my_recipe_box/lib/converter.dart
+++ flutter_project/my_recipe_box/lib/converter.dart
@@ -19,11 +19,7 @@ class _ConverterState extends State<Converter> {
 
 
     // TODO: check that amountEntry is a valid input (only numeric or period). if not, return "INVALID AMOUNT VALUE"
-    // TODO: support integer inputs.
     // TODO: any other units to support?
-    // TODO: truncate output to two decimal places
-    // TODO: say the unit type in the output
-    // TODO: fix the button situation (hitting convert isn't what makes it work)
 
 
     double numericAmount = double.parse(amountEntry);
@@ -94,19 +90,12 @@ class _ConverterState extends State<Converter> {
     }
 
 
-    String result = numericResult.toString(); // TODO: cast from numericResult
+    String result = numericResult.toStringAsFixed(2) + " " + secondDropdownValue;
     return result;
   }
 
   @override
   Widget build(BuildContext context) {
-
-
-
-
-    //List<String> units = <String>["Unit Placeholder 1", "Unit Placeholder 2", "Unit Placeholder 3"]; // TODO: add more
-
-
     return Container(
       color: Colors.green[50], // change this color to change the background color
 
@@ -116,157 +105,135 @@ class _ConverterState extends State<Converter> {
 
         mainAxisSize: MainAxisSize.min,
         children: <Widget>[
-/*
-            ListTile(
-              // contentPadding: , // TODO: add padding
 
-              //leading: Icon(Icons.local_dining), // NOTE: could add an icon here
-              //title: Text("\n${units[index]}"),
-              subtitle: Text(
-                  "\nEnter the amount you would like to convert, select your units, then hit 'Convert'",
-                  style: TextStyle(color: Colors.black, fontWeight: FontWeight.bold, fontSize: 20),
-                  textAlign: TextAlign.center
-              ),
+        ListTile( // NOTE: DROPDOWN 1
+            title: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: <Widget> [
 
-            ),*/
+                  Padding(
+                    padding: EdgeInsets.all(30.0),
+                    child: Container(
 
-            ListTile( // NOTE: DROPDOWN 1
-                title: Column(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: <Widget> [
+                      width: 100.0,
+                      child: TextFormField( // NOTE: text entry for amount
 
-                      Padding(
-                        padding: EdgeInsets.all(30.0),
-                        child: Container(
-
-                          width: 100.0,
-                          child: TextFormField( // NOTE: text entry for amount
-
-                              decoration: InputDecoration(
-                                border: new OutlineInputBorder(borderSide: new BorderSide()),
-                                labelText: 'Amount:'
-                              ),
-                              keyboardType: TextInputType.numberWithOptions(decimal: true),
-                              onChanged: (newValue) {
-                                setState(() {
-                                  amountEntry = newValue;
-                                });
-                              },
-                          ),
-
+                        decoration: InputDecoration(
+                          border: new OutlineInputBorder(borderSide: new BorderSide()),
+                          labelText: 'Amount:'
                         ),
-                      ),
-
-
-
-
-                      DropdownButton(
-
-                        value: firstDropdownValue,
-                        //icon: Icon(Icons.arrow_downward), NOTE: could change the icon in the dropdown here
-                        iconSize: 24,
-                        elevation: 16,
-
-                        style: TextStyle(color: Colors.black), // NOTE: This is the color of the dropdown menu text
-                        underline: Container(
-                          height: 2,
-                          color: Colors.green, // NOTE: this is the color of the underline of the dropdown
-                        ),
-                        onChanged: (String newValue) {
+                        keyboardType: TextInputType.numberWithOptions(decimal: true),
+                        onChanged: (newValue) {
                           setState(() {
-                            firstDropdownValue = newValue;
+                            amountEntry = newValue;
                           });
                         },
-                        items: <String>['Ounces', 'Teaspoons', 'Tablespoons', 'Cups', 'Pints', 'Quarts', 'Gallons'].map<DropdownMenuItem<String>>((String value) {
-                          return DropdownMenuItem<String>(
-                            value: value,
-                            child: Text(value),
-                          );
-                        }).toList(),
                       ),
 
-                    ]
-                )
-
-            ),
-
-
-
-
-
-            ListTile( // NOTE: DOWN ARROW
-              title: Icon(
-                  Icons.arrow_downward,
-                  color: Colors.black,
-              ),
-            ),
-            ListTile( // NOTE: DROPDOWN 2
-                title: Column(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: <Widget> [
-                    DropdownButton(
-
-                      value: secondDropdownValue,
-                      //icon: Icon(Icons.arrow_downward), NOTE: could change the icon in the dropdown here
-                      iconSize: 24,
-                      elevation: 16,
-
-                      style: TextStyle(color: Colors.black), // NOTE: This is the color of the dropdown menu text
-                      underline: Container(
-                        height: 2,
-                        color: Colors.green, // NOTE: this is the color of the underline of the dropdown
-                      ),
-                      onChanged: (String newValue) {
-                        setState(() {
-                          secondDropdownValue = newValue;
-                        });
-                      },
-                      items: <String>['Ounces', 'Teaspoons', 'Tablespoons', 'Cups', 'Pints', 'Quarts', 'Gallons'].map<DropdownMenuItem<String>>((String value) {
-                        return DropdownMenuItem<String>(
-                          value: value,
-                          child: Text(value),
-                        );
-                      }).toList(),
                     ),
+                  ),
 
+                  DropdownButton(
 
-                    Padding(
-                      padding: EdgeInsets.all(30.0),
-                      child: RaisedButton(
-                          onPressed: () {
-                            resultValue = unitConversion(amountEntry, firstDropdownValue, secondDropdownValue);
-                          },
-                          textColor: Colors.white,
-                          color: Colors.red[400],
-                          padding: const EdgeInsets.all(0.0),
-                          child: Container(
-                            decoration: const BoxDecoration(
-                            ),
-                            padding: const EdgeInsets.all(10.0),
-                            child:
-                            const Text('Convert', style: TextStyle(fontSize: 16)),
-                          ),
-                        ),
+                    value: firstDropdownValue,
+                    //icon: Icon(Icons.arrow_downward), NOTE: could change the icon in the dropdown here
+                    iconSize: 24,
+                    elevation: 16,
+
+                    style: TextStyle(color: Colors.black), // NOTE: This is the color of the dropdown menu text
+                    underline: Container(
+                      height: 2,
+                      color: Colors.green, // NOTE: this is the color of the underline of the dropdown
                     ),
+                    onChanged: (String newValue) {
+                      setState(() {
+                        firstDropdownValue = newValue;
+                      });
+                    },
+                    items: <String>['Ounces', 'Teaspoons', 'Tablespoons', 'Cups', 'Pints', 'Quarts', 'Gallons'].map<DropdownMenuItem<String>>((String value) {
+                      return DropdownMenuItem<String>(
+                        value: value,
+                        child: Text(value),
+                      );
+                    }).toList(),
+                  ),
 
+                ]
+            )
 
-                    RichText( // NOTE: result  here
-                      text: TextSpan(text: '', style: DefaultTextStyle.of(context).style,
-                        children: <TextSpan>[
-                          TextSpan(text: resultValue, style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold)),
-                        ],
-                      ),
-                    )
+        ),
 
-
-
-                  ]
-              )
-
+        ListTile( // NOTE: DOWN ARROW
+          title: Icon(
+              Icons.arrow_downward,
+              color: Colors.black,
           ),
+        ),
+        ListTile( // NOTE: DROPDOWN 2
+            title: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: <Widget> [
+                DropdownButton(
+
+                  value: secondDropdownValue,
+                  //icon: Icon(Icons.arrow_downward), NOTE: could change the icon in the dropdown here
+                  iconSize: 24,
+                  elevation: 16,
+
+                  style: TextStyle(color: Colors.black), // NOTE: This is the color of the dropdown menu text
+                  underline: Container(
+                    height: 2,
+                    color: Colors.green, // NOTE: this is the color of the underline of the dropdown
+                  ),
+                  onChanged: (String newValue) {
+                    setState(() {
+                      secondDropdownValue = newValue;
+                    });
+                  },
+                  items: <String>['Ounces', 'Teaspoons', 'Tablespoons', 'Cups', 'Pints', 'Quarts', 'Gallons'].map<DropdownMenuItem<String>>((String value) {
+                    return DropdownMenuItem<String>(
+                      value: value,
+                      child: Text(value),
+                    );
+                  }).toList(),
+                ),
+
+
+                Padding(
+                  padding: EdgeInsets.all(30.0),
+                  child: RaisedButton(
+                      onPressed: () {
+                        setState(() {
+                          resultValue = unitConversion(amountEntry, firstDropdownValue, secondDropdownValue);
+                        });
+
+                      },
+                      textColor: Colors.white,
+                      color: Colors.red[400],
+                      padding: const EdgeInsets.all(0.0),
+                      child: Container(
+                        decoration: const BoxDecoration(
+                        ),
+                        padding: const EdgeInsets.all(10.0),
+                        child:
+                        const Text('Convert', style: TextStyle(fontSize: 16)),
+                      ),
+                    ),
+                ),
+
+
+                RichText( // NOTE: result  here
+                  text: TextSpan(text: '', style: DefaultTextStyle.of(context).style,
+                    children: <TextSpan>[
+                      TextSpan(text: resultValue, style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold)),
+                    ],
+                  ),
+                )
+              ]
+            )
+        ),
         ],
       ),
-
     );
   }
 }
