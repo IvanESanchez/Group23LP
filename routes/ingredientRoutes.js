459133ccad01015655f956681c2de75489925de0
Spirit Xela <morse.alex.j@gmail.com>
2020-07-15 17:15:41 -0400

    Changed get all ingredients to GET request

--- routes/ingredientRoutes.js
+++ routes/ingredientRoutes.js
@@ -6,7 +6,7 @@ const authController = require('./../controllers/authController');
 // mergeParams to get access the parameters of the routes and merge them
 const router = express.Router({ mergeParams: true });
 
-router.route('/list').post(authController.protect, ingredientController.getAllIngredients);
+router.route('/list').get(authController.protect, ingredientController.getAllIngredients);
 router.route('/addIngredient').post(authController.protect, ingredientController.createIngredient);
 router.route('/removeIngredient').post(authController.protect, ingredientController.deleteIngredient);
 
