cfffaa838b6e3ee13d56cbf6a82ae3b577fb544c
Julian Braha <julianbraha@gmail.com>
2020-07-16 17:58:13 -0400

    Added download icon to 'Get Recipe' buttons

--- flutter_project/my_recipe_box/lib/getrecipes.dart
+++ flutter_project/my_recipe_box/lib/getrecipes.dart
@@ -17,37 +17,37 @@ class GetRecipes extends StatelessWidget {
       //backgroundColor: Colors.white,
       child: Scaffold(
         backgroundColor: Colors.green[50],
-    // Appbar
-    appBar: AppBar( // NOTE: AppBar is the header of the app
-    // Title
-    // TODO: use this link to change the title dynamically: https://stackoverflow.com/questions/52333151/how-to-change-the-app-bar-title-in-flutter
-    title: Text(
-      //userName + spacing + formattedDate,
-      formattedDate
-    ),
+        // Appbar
+        appBar: AppBar( // NOTE: AppBar is the header of the app
+        // Title
+        // TODO: use this link to change the title dynamically: https://stackoverflow.com/questions/52333151/how-to-change-the-app-bar-title-in-flutter
+          title: Text(
+            //userName + spacing + formattedDate,
+            formattedDate
+          ),
 
-    centerTitle: true,
-    backgroundColor: Colors.green,  // Set the background color of the App Bar
-    actions: <Widget>[
-    // action button
-      // TODO: uncomment to add button
-    /*IconButton(
-    icon: Icon(choices[0].icon),
-    tooltip: "My Profile",
-    onPressed: () {
-    //_select(choices[0]);
-    Navigator.push(
-    context,
-    MaterialPageRoute(builder: (context) => MyProfile()),
-    );
-    },
-    ),*/
-    // action button
-    // overflow menu
+          centerTitle: true,
+          backgroundColor: Colors.green,  // Set the background color of the App Bar
+          actions: <Widget>[
+          // action button
+            // TODO: uncomment to add button
+          /*IconButton(
+          icon: Icon(choices[0].icon),
+          tooltip: "My Profile",
+          onPressed: () {
+          //_select(choices[0]);
+          Navigator.push(
+          context,
+          MaterialPageRoute(builder: (context) => MyProfile()),
+          );
+          },
+          ),*/
+          // action button
+          // overflow menu
 
-    ],
-    ),
-    // Set the TabBar view as the body of the Scaffold
+          ],
+        ),
+        // Set the TabBar view as the body of the Scaffold
 
         body: ListView.separated(
           //padding: const EdgeInsets.only(top: 50, left: 10, right: 10),
@@ -68,8 +68,9 @@ class GetRecipes extends StatelessWidget {
                     ),
                     ButtonBar(
                       children: <Widget>[
-                        FlatButton(
-                          child: const Text("Get Recipe"),
+                        FlatButton.icon(
+                          icon: const Icon(Icons.file_download), // NOTE: button icon
+                          label: Text("Get Recipe"),
                           color: Colors.red[400],
                           onPressed: () {
                             // NOTE: this is what happens when the "Get Recipe" button is pressed
