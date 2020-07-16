f7b3fcef6813f13e8d319b45517441a8c5442339
ChandlerEarp <chandlerearp99@gmail.com>
2020-07-16 17:31:14 -0400

    Update calendarRoutes.js

--- routes/calendarRoutes.js
+++ routes/calendarRoutes.js
@@ -5,8 +5,8 @@ const calendarController = require('../controllers/calendarController');
 
 const router = express.Router();
 
-router
-  .route('/')
-  .post(authController.protect, calendarController.createCalendar);
+  router.route('/').post(authController.protect, calendarController.createCalendar);
+  router.route('/addToCalendar').post(authController.protect, calendarController.addRecipe);
+  router.route('/deleteFromCalendar').post(authController.protect, calendarController.deleteRecipe);
 
 module.exports = router;
