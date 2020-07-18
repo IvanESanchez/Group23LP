a7767eb7f98ae158d71ff91f5fc71229acdeeaad
Julian Braha <julianbraha@gmail.com>
2020-07-18 18:59:33 -0400

    Ingredients can now be added to a new recipe

--- flutter_project/my_recipe_box/lib/createrecipe.dart
+++ flutter_project/my_recipe_box/lib/createrecipe.dart
@@ -1,37 +1,98 @@
 import 'package:flutter/material.dart';
 
+import 'package:my_recipe_box/main.dart';
+
 import 'package:intl/intl.dart'; // necessary for getting the current date for the header bar
 
+class Ingredient {
+  String name;
+  String amount;
+  String unit;
+}
 
 
+Ingredient currentIngredient;
+
+
+List<Ingredient> _ingredients = [];
 // creates a concrete implementation of _CreateRecipe. effectively just a wrapper
 class CreateRecipe extends StatefulWidget {
   CreateRecipe({Key key}) : super(key: key);
-
-
-
   @override
   _CreateRecipe createState() => _CreateRecipe();
 }
 
 
+
 // this is abstract, but it's where the real magic happens.
 class _CreateRecipe extends State<CreateRecipe> {
-  List<String> _ingredients = [];
 
-  void _addIngredient(String ingredient) {
+  Widget buildIngredientList() {
+    return new ListView.builder(
+      itemBuilder: (context, index) {
+        if (index < _ingredients.length) {
+          return _buildIngredientList(_ingredients[index], index);
+        } else return null;
+      },
+    );
+  }
+
+  Widget _buildIngredientList(Ingredient ingredient, int index) {
+    return new ListTile(
+        title: new Text(ingredient.name),
+        onTap: () => _promptRemoveIngredient(index)
+    );
+  }
+
+  // Much like _addTodoItem, this modifies the array of todo strings and
+// notifies the app that the state has changed by using setState
+  void _removeTodoItem(int index) {
+    setState(() => _ingredients.removeAt(index));
+  }
+
+// Show an alert dialog asking the user to confirm that the task is done
+  void _promptRemoveIngredient(int index) {
+    showDialog(
+        context: context,
+        builder: (BuildContext context) {
+          return new AlertDialog(
+              title: new Text('Mark "${_ingredients[index]}" as done?'),
+              actions: <Widget>[
+                new FlatButton(
+                    child: new Text('CANCEL'),
+                    onPressed: () => Navigator.of(context).pop()
+                ),
+                new FlatButton(
+                    child: new Text('MARK AS DONE'),
+                    onPressed: () {
+                      _removeTodoItem(index);
+                      Navigator.of(context).pop();
+                    }
+                )
+              ]
+          );
+        }
+    );
+  }
+
+
+  void _addIngredient(Ingredient ingredient) {
     // Only add the ingredient if the user actually entered something
-    if (ingredient.length > 0) {
+    if (ingredient.name.length > 0 && ingredient.amount.length > 0 && ingredient.unit.length > 0) {
       setState(() => _ingredients.add(ingredient));
     }
+    print("ingredients is " );
+    print(_ingredients);
   }
+
 
   @override
   Widget build(BuildContext context) {
 
     final _formKey = GlobalKey<FormState>();
-    DateTime now = DateTime.now();
-    String formattedDate = DateFormat("EEEEEEEE, MMM dd").format(now); // NOTE: the month text will be truncated to 4 characters long.
+    //DateTime now = DateTime.now();
+    //String formattedDate = DateFormat("EEEEEEEE, MMM dd").format(now); // NOTE: the month text will be truncated to 4 characters long.
+    String formattedDate = "Recipe Creator";
 
     return Scaffold(
       body: Container(
@@ -70,13 +131,18 @@ class _CreateRecipe extends State<CreateRecipe> {
 
                         TextFormField(
                           decoration: const InputDecoration(
-                            hintText: 'Enter the recipe title here',
+                            hintText: 'Recipe Title',
                           ),
                           validator: (value) {
                             if (value.isEmpty) {
                               return 'Please enter some text';
                             }
                             return null;
+                          },
+                          onChanged: (newValue) {
+                            setState(() {
+                              //TODO set the title here
+                            });
                           },
                         ),
 
@@ -87,13 +153,18 @@ class _CreateRecipe extends State<CreateRecipe> {
                           padding: const EdgeInsets.symmetric(vertical: 32.0),
                           child: TextFormField(
                             decoration: const InputDecoration(
-                            hintText: 'Enter the instructions here',
+                            hintText: 'Instructions',
                             ),
                             validator: (value) {
                               if (value.isEmpty) {
                                return 'Please enter some text';
                               }
                               return null;
+                            },
+                            onChanged: (newValue) {
+                              setState(() {
+                               // TODO: set the instructions here
+                              });
                             },
                           ),
                         ),
@@ -102,6 +173,9 @@ class _CreateRecipe extends State<CreateRecipe> {
                       ],
                     ),
                   ),
+
+                  // TODO: add a card where the ingredient list will go
+                  // TODO: add buttons and interface for adding ingredient
 
 
                   // TODO: add a pop-up menu for adding an ingredient: https://api.flutter.dev/flutter/material/PopupMenuButton-class.html
@@ -116,16 +190,87 @@ class _CreateRecipe extends State<CreateRecipe> {
 
 
 
-        )
-        ]
         ),
+
+          //buildIngredientList(), // TODO: figure out why this breaks
+
+
+          ListTile(
+            title: Padding(
+              padding: const EdgeInsets.only(bottom: 50.0),
+                child: RaisedButton.icon(
+                  onPressed: () {
+                    _pushIngredient();
+                    // Add your onPressed code here!
+
+                    // TODO: add the actual recipe
+                  },
+                  label: Text("Add Ingredient"),
+                  icon: Icon(Icons.add),
+                  color: Colors.greenAccent,
+                )
+            )
+          ),
+          ListTile(
+              title: Padding(
+                  padding: const EdgeInsets.only(bottom: 50.0),
+          child: DropdownButtonFormField(
+
+            //value: firstDropdownValue,
+            //icon: Icon(Icons.arrow_downward), NOTE: could change the icon in the dropdown here
+            iconSize: 24,
+            elevation: 16,
+
+            style: TextStyle(color: Colors.black), // NOTE: This is the color of the dropdown menu text
+            decoration: InputDecoration(labelText: 'View Current Ingredients'),
+            onChanged: (String newValue) {
+              setState(() {
+                _ingredients.remove(newValue);
+                //firstDropdownValue = newValue;
+              });
+            },
+            items: _ingredients.map<DropdownMenuItem<String>>((Ingredient value) {
+              String allIngredientInfo = value.amount + " " + value.unit + "(s) of " + value.name;
+              return DropdownMenuItem<String>(
+                value: allIngredientInfo,
+                child: Text(allIngredientInfo),
+              );
+            }).toList(),
+          ),
+
+              )
+          ),
+          ListTile(
+              title: Padding(
+                  padding: const EdgeInsets.only(bottom: 50.0),
+                  child: RaisedButton.icon(
+                    onPressed: () {
+                      setState(() => _ingredients.clear());
+
+                    },
+                    label: Text("Clear Ingredients", style: TextStyle(color: Colors.white)),
+                    icon: Icon(Icons.remove),
+                    color: Colors.red[400],
+
+                  )
+              )
+          ),
+        ]
+            )
+            )
+          ),
+
+
 
         floatingActionButton: Padding(
           padding: const EdgeInsets.only(bottom: 50.0),
           child: FloatingActionButton(
             onPressed: () {
-              // Add your onPressed code here!
-              // TODO: add the actual recipe
+              Navigator.push(
+                context,
+                MaterialPageRoute(builder: (context) => MyHome()),
+              );
+              // TODO: add the recipe to the user's list, as well as everyones' "get recipes"
             },
             child: Icon(Icons.check),
             backgroundColor: Colors.greenAccent,
@@ -133,8 +278,155 @@ class _CreateRecipe extends State<CreateRecipe> {
           ),
         ),
         floatingActionButtonLocation: FloatingActionButtonLocation.centerDocked,
-        ),
-      )
+        );
+
+  }
+  void _pushIngredient() {
+    currentIngredient = new Ingredient();
+
+    GlobalKey _formKey;
+    // Push this page onto the stack
+    Navigator.of(context).push(
+      // MaterialPageRoute will automatically animate the screen entry, as well
+      // as adding a back button to close it
+        new MaterialPageRoute(
+            builder: (context) {
+              return new Scaffold(
+                resizeToAvoidBottomPadding: false,
+                  appBar: new AppBar(
+                      title: new Text('Add an Ingredient')
+                  ),
+                  body: Form(
+                    key: _formKey,
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: <Widget>[
+
+                    ListTile(
+                    title: Padding(
+                        padding: const EdgeInsets.only(bottom: 50.0),
+                    child:
+                    TextFormField(
+                          decoration: const InputDecoration(
+                            hintText: 'Ingredient name',
+                          ),
+                          validator: (value) {
+                            if (value.isEmpty) {
+                              return 'Please enter some text';
+                            }
+                            return null;
+                          },
+                          onChanged: (newValue) {
+                            setState(() {
+                              currentIngredient.name = newValue;
+                            });
+                          },
+                        ),
+                  )
+                    ),
+              ListTile(
+                title: Padding(
+                  padding: const EdgeInsets.only(bottom: 50.0),
+                  child:
+
+                  Padding(
+                          padding: const EdgeInsets.symmetric(vertical: 32.0),
+                          child: TextFormField(
+                            decoration: const InputDecoration(
+                              hintText: 'Ingredient unit',
+                            ),
+                            validator: (value) {
+                              if (value.isEmpty) {
+                                return 'Please enter some text';
+                              }
+                              return null;
+                            },
+                            onChanged: (newValue) {
+                              setState(() {
+                                currentIngredient.unit = newValue;
+                              });
+                            },
+                          ),
+                        ),
+                )
+              ),
+
+              ListTile(
+                title: Padding(
+                  padding: const EdgeInsets.only(bottom: 50.0),
+                  child:
+                  Padding(
+                          padding: const EdgeInsets.symmetric(vertical: 32.0),
+                          child: TextFormField(
+                            decoration: const InputDecoration(
+                              hintText: 'Ingredient amount',
+                            ),
+                            validator: (value) {
+                              if (value.isEmpty) {
+                                return 'Please enter some text';
+                              }
+                              return null;
+                            },
+                            onChanged: (newValue) {
+                              setState(() {
+                                currentIngredient.amount = newValue;
+                              });
+                            },
+
+                          ),
+                        ),
+                )
+              ),
+              ListTile(
+                title: Padding(
+                  padding: const EdgeInsets.only(bottom: 50.0),
+                  child:
+                  RaisedButton.icon(
+                          label: Text('Add', style: TextStyle(
+                              color: Colors.black
+                          )),
+                          color: Colors.greenAccent,
+                          icon: const Icon(Icons.add), // NOTE: button icon
+                          onPressed:() {
+                            _addIngredient(currentIngredient);
+                            //print("ingredient name is ");
+                            //print(ingredientName);
+                            Navigator.pop(context); // Close the add todo screen
+                          }
+
+                        )
+                )
+              )
+
+                      ],
+                    ),
+                  ),
+
+
+
+
+
+
+
+
+
+
+                  /*
+                  new TextField(
+                    autofocus: true,
+                    onSubmitted: (val) {
+                      _addIngredient(val);
+                      Navigator.pop(context); // Close the add todo screen
+                    },
+                    decoration: new InputDecoration(
+                        hintText: 'Enter ingredient name',
+                        contentPadding: const EdgeInsets.all(16.0)
+                    ),
+
+                  )*/
+              );
+            }
+        )
     );
   }
 }
