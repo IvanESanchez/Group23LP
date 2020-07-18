947f985162e1974f456c5fd0b62b22f0f0fb90a0
amberhoekstra <hoekstra.amber@knights.ucf.edu>
2020-07-17 21:30:21 -0400

    updated calendar and today's recipes

--- flutter_project/my_recipe_box/lib/todaysrecipes.dart
+++ flutter_project/my_recipe_box/lib/todaysrecipes.dart
@@ -1,33 +1,100 @@
 import 'package:flutter/material.dart';
+import 'package:my_recipe_box/getrecipes.dart';
+import 'package:my_recipe_box/myrecipes.dart';
 
-class TodaysRecipes extends StatelessWidget {
+class MyRecipes extends StatelessWidget {
+
+
   @override
   Widget build(BuildContext context) {
+    List<String> recipes = <String>["Recipe Placeholder 1", "Recipe Placeholder 2", "Recipe Placeholder 3"];
+    //List<String> ingredients = <String>['Ingredient Placeholder 1', 'Recipe Placeholder 2', 'Recipe Placeholder 3'];
     return Container(
         color: Colors.green[50], // change this color to change the background color
-        child: Scaffold(
-          //backgroundColor: Colors.white,
-          body: Container(
-            color: Colors.green[50], // change this color to change the background color
-            child: Center(
-              child: Column(
-                // center the children
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: <Widget>[
-                  Icon(
-                    Icons.today,
-                    size: 160.0,
-                    color: Colors.grey,
+        //backgroundColor: Colors.white,
+
+        child: new Scaffold (
+            backgroundColor: Colors.green[50],
+            body: ListView.separated(
+              padding: const EdgeInsets.all(10),
+              itemCount: recipes.length,
+              separatorBuilder: (BuildContext context, int index) => const Divider(),
+              itemBuilder: (BuildContext context, int index) {
+                return Container(
+                  child: Card(
+
+                    child: Column(
+
+
+                      mainAxisSize: MainAxisSize.min,
+                      children: <Widget>[
+                        ListTile(
+
+                          leading: Icon(Icons.local_dining),
+                          title: Text("\n${recipes[index]}"),
+                          subtitle: Text("More information can go here, such as author name, or ingredients"),
+                        ),
+                        ButtonBar(
+                          children: <Widget>[
+                            FlatButton(
+                              child: const Text("View Recipe"),
+                              color: Colors.red[400],
+                              onPressed: () {
+                                // NOTE: this is what happens when the "Get Recipe" button is pressed
+                              },
+                            ),
+                            /*FlatButton(
+                       child: const Text('another button could go here'),
+                       onPressed: () {/* ... */},
+                     ), */
+                          ],
+                        ),
+
+                      ],
+                    ),
                   ),
-                  Text(
-                    "Today's Recipes",
-                    style: TextStyle(color: Colors.grey),
-                  )
-                ],
-              ),
+
+                );
+
+
+              },
+
             ),
-          ),
+            floatingActionButton: FloatingActionButton.extended(
+              elevation: 0.0,
+              icon: const Icon(Icons.add), // NOTE: button icon
+              backgroundColor: Colors.red[400], // NOTE: button color
+              onPressed: ()
+              {
+                // TODO: open the getrecipes.dart
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(builder: (context) => GetRecipes()),
+                );
+              },
+              label: Text('New Recipe'),
+            )
         )
+
+
+
+
+
     );
   }
+
+
 }
+
+/*
+
+        FloatingActionButton.extended(
+          onPressed: () {
+            // Add your onPressed code here!
+          },
+          label: Text('Approve'),
+          icon: Icon(Icons.thumb_up),
+          backgroundColor: Colors.pink,
+        ),
+ */
+
